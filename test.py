959a2f8c1dd5492e39809313985319aa131dc46e
JunmoJeong <jjmstars7@gmail.com>
2022-06-01 10:00:28 +0900

    flask

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/hello")
 def abc():
     return "<h1>Hello World!</h1>"
+
+
+host_addr = "127.0.0.1"
+port_num = "8080"
+
+if __name__ == "__main__":
+    app.run(host=host_addr, port=port_num, debug=True)
