bc25efd65cbb0d14a6c08902caac2c2415b79f3f
JunmoJeong <jjmstars7@gmail.com>
2022-06-16 22:28:35 +0900

    test

--- test2.py
+++ test2.py
@@ -29,8 +29,21 @@ num_list = [1, 2, 3, 4, 5]
 list_square(calc_square, num_list)
 '''
 
-
+'''
 def html_crate(tag):
-    def text_wrapper(msg):
-        print('<{0}>{1}</{0}>'.format(tag, msg))
-    return text_wrapper
+    def text_write(msg):
+        print('{0}+" "+{1}'.format(tag, msg))
+    return text_write
+
+data_
+'''
+
+
+import requests
+from bs4 import BeautifulSoup
+res = requests.get("https://davelee-fun.github.io/blog/crawl_html_css.html")
+soup = BeautifulSoup(res.content, 'html.parser')
+# a 태그이면서 href 속성 값이 특정한 값을 갖는 경우 탐색
+link_titles = soup.select("ul#hobby_course_list > li")
+for link_title in link_titles:
+    data_list_minus(link_title.get_text())
